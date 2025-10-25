33f42b5ced52745de39bce2b7738f766c8e0eb8b
ansleybrown1337 <ansleybrown1337@gmail.com>
2025-10-25 13:19:48 -0600

    Update dag2_23July2025.r

--- code/dag2_23July2025.r
+++ code/dag2_23July2025.r
@@ -3,8 +3,9 @@ bb="-5.892,-5.356,4.412,6.847"
 "Conc. e" [latent,pos="-4.568,-3.064"]
 "Crop Type" [exposure,pos="1.574,-0.546"]
 "Depth measurement method" [exposure,pos="0.177,3.977"]
-"Inflow Conc." [exposure,pos="-2.020,-4.208"]
+"Inflow Conc." [exposure,pos="-1.484,-3.787"]
 "Inflow vol" [exposure,pos="-1.289,-0.605"]
+"Irrigation Method" [exposure,pos="0.375,1.380"]
 "Load e." [latent,pos="-5.102,0.612"]
 "Outflow Conc." [outcome,pos="-3.089,-2.367"]
 "Outflow vol" [outcome,pos="-1.647,2.641"]
@@ -21,9 +22,11 @@ STIR [exposure,pos="0.252,-3.050"]
 "Crop Type" -> "Soil Properties"
 "Depth measurement method" -> "Outflow vol"
 "Inflow Conc." -> "Outflow Conc."
+"Inflow Conc." -> "Soil Properties"
 "Inflow vol" -> "Outflow Conc."
 "Inflow vol" -> "Outflow vol"
 "Inflow vol" -> "Soil Properties"
+"Irrigation Method" -> "Inflow vol"
 "Load e." -> Load
 "Outflow Conc." -> Load
 "Outflow vol" -> Load
